704eafc0caec8fe599d5a75c14865ef814a65b1c
thatGui <thatguifonseca@gmail.com>
2024-06-10 17:42:43 +0100

    css reset plus layout

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+
+import './Button.css';
+
+type btnProps = {
+	btnText: string;
+	btnonClick: () => void;
+	btnclassName: string;
+};
+
+export default function Button({
+	btnText,
+	btnonClick,
+	btnclassName,
+}: btnProps) {
+	return (
+		<button className={btnclassName} onClick={btnonClick}>
+			{btnText}
+		</button>
+	);
+}
