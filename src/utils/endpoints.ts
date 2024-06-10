3c201c8788326a18f5eda11b01881ea13e662b13
thatGui <thatguifonseca@gmail.com>
2024-06-10 21:02:50 +0100

    anotherOne

---
+++ src/utils/endpoints.ts
@@ -0,0 +1,47 @@
+export const deleteProduct = async (id: number) => {
+	try {
+		const response = await fetch(`http://localhost:????/product/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		if (error instanceof Error) {
+			alert(error.message);
+		} else {
+			alert('An unexpected error occurred');
+		}
+	}
+};
+
+export const singleProduct = async (id: number) => {
+	try {
+		const response = await fetch(`http://localhost:????/product/${id}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		if (error instanceof Error) {
+			alert(error.message);
+		} else {
+			alert('An unexpected error occurred');
+		}
+	}
+};
