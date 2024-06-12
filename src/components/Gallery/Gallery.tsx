8f38d2321e1ac5a26243a0709f478ed2d9387520
AlexVOiceover <alex@alexvoiceover.com>
2024-06-12 17:31:09 +0100

    Created Gallery component

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,38 @@
-//import React from 'react';
+import React from 'react';
+import Card from '../Card/Card';
 import './Gallery.css';
 
-export default function Gallery() {
-	return <div>Gallery</div>;
-}
+type Product = {
+  image?: string;
+  name: string;
+  description?: string;
+  price: number;
+};
+
+type GalleryProps = {
+  products: Product[];
+};
+
+/**
+ * Gallery Component
+ * 
+ * @param {Product[]} products - An array of product objects.
+ * @returns {JSX.Element} The Gallery component displaying a list of product cards.
+ */
+const Gallery: React.FC<GalleryProps> = ({ products }) => {
+  return (
+    <div className="gallery">
+      {products.map((product, index) => (
+        <Card
+          key={index}
+          image={product.image}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
