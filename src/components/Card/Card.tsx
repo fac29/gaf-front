f9e527774c81af9dc48fb5145d268dad2efaf760
AlexVOiceover <alex@alexvoiceover.com>
2024-06-12 17:18:27 +0100

    Added JSDoc to the component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,39 @@
-//import React from 'react'
 import './Card.css';
-import Imgdisplay from '../ImgDisplay/ImgDisplay';
+import ImgDisplay from '../ImgDisplay/ImgDisplay';
 
-export default function Card() {
+type CardProps = {
+	image?: string;
+	name: string;
+	description?: string;
+	price: number;
+};
+
+/**
+ * Card Component
+ *
+ * @param {string} [image] - The URL or path of the product image. Optional.
+ * @param {string} name - The name of the product.
+ * @param {string} [description] - A brief description of the product. Optional.
+ * @param {number} price - The price of the product.
+ * @returns {JSX.Element} The Card component displaying product details.
+ */
+export default function Card({
+	image,
+	name,
+	description,
+	price,
+}: CardProps): JSX.Element {
 	return (
-		<div>
-			Card
-			<Imgdisplay />
+		<div className="card">
+			<ImgDisplay
+				size={'medium'}
+				image={image || '../Images/placeholder-image.jpg'}
+			/>
+			<div className="card-details">
+				<h2>{name}</h2>
+				{description && <p>{description}</p>}
+				<p className="card-price">£{price.toFixed(2)}</p>
+			</div>
 		</div>
 	);
 }
