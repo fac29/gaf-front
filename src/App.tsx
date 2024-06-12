58128c63fa690087df6f322494a07a680b78a422
AlexVOiceover <alex@alexvoiceover.com>
2024-06-12 16:32:57 +0100

    Created ImgDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //import React from 'react';
 import Landingpage from './components/Landingpage/Landingpage';
 import ProductPage from './components/Productpage/ProductPage';
+import ImgDisplay from './components/ImgDisplay/ImgDisplay';
 
 export default function App() {
 	return (
 		<>
 			<Landingpage />
 			<ProductPage />
+			<ImgDisplay size={'small'} image={'/Images/image.jpeg'} />
 		</>
 	);
 }
