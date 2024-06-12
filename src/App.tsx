64be0b6b7f454b4451dae9f2819883f5d49a973b
AlexVOiceover <alex@alexvoiceover.com>
2024-06-12 16:44:11 +0100

    removed extra import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //import React from 'react';
 import Landingpage from './components/Landingpage/Landingpage';
 import ProductPage from './components/Productpage/ProductPage';
-import ImgDisplay from './components/ImgDisplay/ImgDisplay';
+//import ImgDisplay from './components/ImgDisplay/ImgDisplay';
 
 export default function App() {
 	return (
