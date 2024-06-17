df0218183ae1ebc886495c69c7d9929e5cda0e87
thatGui <thatguifonseca@gmail.com>
2024-06-17 14:21:45 +0100

    commit img

--- src/components/ImgDisplay/ImgDisplay.tsx
+++ src/components/ImgDisplay/ImgDisplay.tsx
@@ -3,5 +3,18 @@ import './ImgDisplay.css';
 import { ImgPropt } from '../../utils/tyBucket';
 
 export default function ImgDisplay({ imgurl, look }: ImgPropt) {
-	return <img src={imgurl} style={look} />;
+	if (look === 'thumbnail')
+		return (
+			<img
+				src={imgurl}
+				style={{ width: '256px', height: '256px', aspectRatio: '3/2' }}
+			/>
+		);
+	if (look === 'heroimage')
+		return (
+			<img
+				src={imgurl}
+				style={{ width: '100%', height: '100%', aspectRatio: '3/2' }}
+			/>
+		);
 }
