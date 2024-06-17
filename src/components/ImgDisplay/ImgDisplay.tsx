0b3a3bda240b1405781b4c2d5def2ba9d8b173c9
thatGui <thatguifonseca@gmail.com>
2024-06-17 14:23:07 +0100

    LGTM

--- src/components/ImgDisplay/ImgDisplay.tsx
+++ src/components/ImgDisplay/ImgDisplay.tsx
@@ -14,7 +14,14 @@ export default function ImgDisplay({ imgurl, look }: ImgPropt) {
 		return (
 			<img
 				src={imgurl}
-				style={{ width: '100%', height: '100%', aspectRatio: '3/2' }}
+				style={{ width: '100%', height: '100%', aspectRatio: '21/9' }}
+			/>
+		);
+	if (look === 'cartimage')
+		return (
+			<img
+				src={imgurl}
+				style={{ width: '64px', height: '64px', aspectRatio: '1' }}
 			/>
 		);
 }
