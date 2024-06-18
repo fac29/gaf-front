1ca08de541a68f9dc9e311c8f1a430f8040e51d6
thatGui <thatguifonseca@gmail.com>
2024-06-18 21:25:38 +0100

    nav cart items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import './Navbar.css';
 import Search from '../Search/Search';
 import CreateLog from '../CreateLog/CreateLog';
 import Button from '../Button/Button';
+import { useUserContext } from '../UserContextProvider';
 
 type props = {
 	hasSearch?: boolean;
@@ -17,13 +18,19 @@ export default function Navbar({ hasSearch = true }: props) {
 
 	const openLogInModal = () => setIsLogInOpen(true);
 	const closeLogInModal = () => setIsLogInOpen(false);
+	//calling the customHook for the contextAPI function
+	const { user } = useUserContext();
+	//create a function that checks the user.cart array and tallys the total quantity of items
+	const totalQuantity: number = user.cart.reduce((acc, item) => {
+		return acc + item.quantity;
+	}, 0);
 
 	return (
 		<div className="navstyle">
 			<a href="">Home</a>
 			<div className="rightnav">
 				{hasSearch && <Search />}
-				<a href="">Cart</a>
+				<a href="">Cart {totalQuantity > 0 ? `${totalQuantity}items` : ''}</a>
 				<Button
 					btnText="Create Account"
 					btnclassName="btnSecondary"
