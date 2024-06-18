4c288d4fd503b2479d7d00b3f7e9447a98f4ad03
thatGui <thatguifonseca@gmail.com>
2024-06-18 17:22:50 +0100

    half done

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,10 @@
 import './Card.css';
 import ImgDisplay from '../ImgDisplay/ImgDisplay';
 import Button from '../Button/Button';
-import { Cards } from '../../utils/tyBucket';
+import { Cards, Cart } from '../../utils/tyBucket';
 import { useNavigate } from 'react-router-dom';
 import { useUserContext } from '../UserContextProvider';
+import { CartItem } from '../../utils/tyBucket';
 
 export default function Card({ id, image, name, description, price }: Cards) {
 	const navigate = useNavigate();
@@ -12,8 +13,23 @@ export default function Card({ id, image, name, description, price }: Cards) {
 	const handleProductRouting = () => {
 		navigate(`/product/${id}`);
 	};
-	const handleAddToBasket = () => {
-		if(user) setUser({ ...user, cart: [...user.cart[]] });
+	const handleAddToCart = (id: string) => {
+		if (user) {
+			const doesItemExist = user.cart.find(
+				(el: CartItem) => el.productId === id,
+			);
+			if (doesItemExist) {
+				doesItemExist.quantity += 1;
+			} else {
+				let newCart = [...user.cart, { productId: id, quantity: 1 }];
+
+				setUser({
+					...user,
+					cart: newCart,
+				});
+			}
+		}
+		console.log(user);
 	};
 
 	return (
@@ -30,13 +46,12 @@ export default function Card({ id, image, name, description, price }: Cards) {
 					</div>
 					<p className="card-price">£{price.toFixed(2)}</p>
 				</div>
-
-				<Button
-					btnText="Add to basket"
-					btnonClick={handleAddToBasket}
-					btnclassName="btnPrimary"
-				/>
 			</div>
+			<Button
+				btnText="Add to basket"
+				btnonClick={handleAddToCart(id)}
+				btnclassName="btnPrimary"
+			/>
 		</div>
 	);
 }
