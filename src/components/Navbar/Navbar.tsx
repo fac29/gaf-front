db3832468c59d3cac0b2d899e80e5f0b87fc990a
Fearg <ferskav@gmail.com>
2024-06-18 11:52:06 +0100

    Added CreateLog component and React state to control if displayed
    Relates #42

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,46 @@
-//import React from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import Search from '../Search/Search';
+import CreateLog from '../CreateLog/CreateLog';
+import Button from '../Button/Button';
 
 type props = {
 	hasSearch?: boolean;
 };
 
-export default function Navbar({ hasSearch = true}: props) {
+export default function Navbar({ hasSearch = true }: props) {
+	const [isCreateAccountOpen, setIsCreateAccountOpen] = useState(false);
+	const [isLogInOpen, setIsLogInOpen] = useState(false);
+
+	const openCreateAccountModal = () => setIsCreateAccountOpen(true);
+	const closeCreateAccountModal = () => setIsCreateAccountOpen(false);
+
+	const openLogInModal = () => setIsLogInOpen(true);
+	const closeLogInModal = () => setIsLogInOpen(false);
+
 	return (
 		<div className="navstyle">
 			<a href="">Home</a>
 			<div className="rightnav">
 				{hasSearch && <Search />}
 				<a href="">Cart</a>
+				<Button
+					btnText="Create Account"
+					btnclassName="btnSecondary"
+					btnonClick={openCreateAccountModal}
+				/>
+				<Button
+					btnText="Log In"
+					btnclassName="btnSecondary"
+					btnonClick={openLogInModal}
+				/>
 			</div>
+			<CreateLog
+				isCreateAccountOpen={isCreateAccountOpen}
+				isLogInOpen={isLogInOpen}
+				closeCreateAccountModal={closeCreateAccountModal}
+				closeLogInModal={closeLogInModal}
+			/>
 		</div>
 	);
 }
