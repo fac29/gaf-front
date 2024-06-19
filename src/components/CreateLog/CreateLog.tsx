61f0812609524a6a5f412ef588faff8fae31dcca
Fearg <ferskav@gmail.com>
2024-06-19 20:55:13 +0100

    Added state to control react form
    Relates #49

--- src/components/CreateLog/CreateLog.tsx
+++ src/components/CreateLog/CreateLog.tsx
@@ -1,7 +1,8 @@
-// import React from 'react';
+import React from 'react';
 import './CreateLog.css';
 import Button from '../Button/Button';
 import { CreateLogProps } from '../../utils/tyBucket';
+import { login, signUp } from '../../utils/endpoints';
 
 export default function CreateLog({
 	isCreateAccountOpen,
@@ -9,6 +10,46 @@ export default function CreateLog({
 	closeCreateAccountModal,
 	closeLogInModal,
 }: CreateLogProps) {
+	const [nameInput, setNameInput] = React.useState('');
+	const [emailInput, setEmailInput] = React.useState('');
+	const [passwordInput, setPasswordInput] = React.useState('');
+	const [isFormValid, setIsFormValid] = React.useState(false);
+
+	const handleSubmitCreate = (e) => {
+		e.preventDefault();
+		if (validateSubmitForm()) {
+			signUp(nameInput, emailInput, passwordInput);
+		} else {
+			alert('Please fill in all required fields.');
+		}
+	};
+	const handleSubmitLogIn = (e) => {
+		e.preventDefault();
+		if (validateLogInForm()) {
+			login(emailInput, passwordInput);
+		} else {
+			alert('Please fill in all required fields.');
+		}
+	};
+
+	const validateSubmitForm = () => {
+		const isNameValid = nameInput.trim().length > 0;
+		const isEmailValid = emailInput.trim().length > 0;
+		const isPasswordValid = passwordInput.trim().length > 0;
+
+		const isValid = isNameValid && isEmailValid && isPasswordValid;
+		setIsFormValid(isValid);
+		return isValid;
+	};
+	const validateLogInForm = () => {
+		const isEmailValid = emailInput.trim().length > 0;
+		const isPasswordValid = passwordInput.trim().length > 0;
+
+		const isValid = isEmailValid && isPasswordValid;
+		setIsFormValid(isValid);
+		return isValid;
+	};
+
 	return (
 		<div className="CreateLog">
 			{isCreateAccountOpen && (
@@ -23,18 +64,49 @@ export default function CreateLog({
 						<h2>Create Account</h2>
 						<form>
 							<div>
-								<label>Name:</label>
-								<input type="text" required />
+								<label>
+									Name:
+									<input
+										type="text"
+										value={nameInput}
+										onChange={(e) => {
+											setNameInput(e.target.value);
+											validateSubmitForm();
+										}}
+									/>
+								</label>
 							</div>
 							<div>
-								<label>Email:</label>
-								<input type="email" required />
+								<label>
+									Email:
+									<input
+										type="email"
+										value={emailInput}
+										onChange={(e) => {
+											setEmailInput(e.target.value);
+											validateSubmitForm();
+										}}
+									/>
+								</label>
 							</div>
 							<div>
-								<label>Password:</label>
-								<input type="password" required />
+								<label>
+									Password:
+									<input
+										type="password"
+										value={passwordInput}
+										onChange={(e) => {
+											setPasswordInput(e.target.value);
+											validateSubmitForm();
+										}}
+									/>
+								</label>
 							</div>
-							<Button btnText="Create Account" btnclassName="btnPrimary" />
+							<Button
+								btnText="Create Account"
+								btnclassName="btnPrimary"
+								btnonClick={handleSubmitCreate}
+							/>
 						</form>
 					</div>
 				</div>
@@ -49,14 +121,36 @@ export default function CreateLog({
 						<h2>Log In</h2>
 						<form>
 							<div>
-								<label>Email:</label>
-								<input type="email" required />
+								<label>
+									Email:
+									<input
+										type="email"
+										value={emailInput}
+										onChange={(e) => {
+											setEmailInput(e.target.value);
+											validateLogInForm();
+										}}
+									/>
+								</label>
 							</div>
 							<div>
-								<label>Password:</label>
-								<input type="password" required />
+								<label>
+									Password:
+									<input
+										type="password"
+										value={passwordInput}
+										onChange={(e) => {
+											setPasswordInput(e.target.value);
+											validateLogInForm();
+										}}
+									/>
+								</label>
 							</div>
-							<Button btnText="Log In" btnclassName="btnPrimary" />
+							<Button
+								btnText="Log In"
+								btnclassName="btnPrimary"
+								btnonClick={handleSubmitLogIn}
+							/>
 						</form>
 					</div>
 				</div>
