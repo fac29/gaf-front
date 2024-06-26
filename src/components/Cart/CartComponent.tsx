f5f6efa3a6dcd669c0a74caedf207413b55945f4
thatGui <thatguifonseca@gmail.com>
2024-06-26 11:56:00 +0100

    added btn

--- src/components/Cart/CartComponent.tsx
+++ src/components/Cart/CartComponent.tsx
@@ -4,6 +4,7 @@ import './CartComponent.css';
 import { useUserContext } from '../UserContextProvider';
 import CartItemComponent from '../CartItemComponent/CartItemComponent';
 import { CartComponentProps } from '../../utils/tyBucket';
+import { fetchCreateCart } from '../../utils/endpoints';
 
 export default function CartComponent({
 	handleModalToggle,
@@ -25,9 +26,26 @@ export default function CartComponent({
 		}
 	};
 
+	const gloryToAFG = async () => {
+		try {
+			const initialPing = await fetchCreateCart(420);
+
+			return console.log('need to extract cartId from here', initialPing);
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			} else {
+				alert('An unexpected error occurred');
+			}
+		}
+	};
+
 	return (
 		<div className="cartycart" onClick={handleClickOutside}>
 			<div className="cartbody">
+				<div>
+					<button onClick={gloryToAFG}>ting</button>
+				</div>
 				{user?.cart && user.cart.length > 0 ? (
 					user.cart.map((item) => (
 						<div key={item.productId} className="cart-item">
