a9be66052db498506fe64e3d1554bbc9df973a6b
Alexander <alex@alexvoiceover.com>
2024-06-26 17:39:11 +0100

    Merged both stacks into one

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,13 +1,20 @@
 // CDK Entry point
 import * as cdk from 'aws-cdk-lib';
-import { BEStack } from '../lib/BEstack';
-import { FEStack } from '../lib/FEStack';
+//import { BEStack } from '../lib/BEstack';
+//import { FEStack } from '../lib/FEStack';
+import { stack } from '../lib/stack';
 
 const app = new cdk.App();
 
-const beStack = new BEStack(app, 'BEStack');
+// const beStack = new BEStack(app, 'BEStack');
 
-new FEStack(app, 'FEStack', {
-  backendUrl: beStack.instancePublicIp,
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
+// new FEStack(app, 'FEStack', {
+//   backendUrl: beStack.instancePublicIp,
+//   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+// });
+new stack(app, 'GAFStack', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+  }); 
